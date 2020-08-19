97ec126abac7ee5b25486359459e761f061de054
caiwenjing <cai_wenjing@outlook.com>
2020-08-19 21:04:53 +0800

    pass L7 nesting

--- src/L7Nesting/Solution.java
+++ src/L7Nesting/Solution.java
@@ -1,4 +1,26 @@
 package L7Nesting;
 
+import java.util.Stack;
+
 public class Solution {
+    public int solution(String S) {
+        int len = S.length();
+        Stack<Character> left_stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (S.charAt(i) == '(') {
+                left_stack.push('(');
+            } else {
+                if (left_stack.empty()) {
+                    return 0;
+                } else {
+                    left_stack.pop();
+                }
+            }
+        }
+        if (left_stack.empty()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
