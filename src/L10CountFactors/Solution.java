fb72b12c7382fe752a7acaef208b5dd1ca700fe1
caiwenjing <cai_wenjing@outlook.com>
2020-08-23 23:11:29 +0800

    Pass L9 MaxSliceSum

---
+++ src/L10CountFactors/Solution.java
@@ -0,0 +1,4 @@
+package L10CountFactors;
+
+public class Solution {
+}
--- src/L9MaxSliceSum/Solution.java
+++ src/L9MaxSliceSum/Solution.java
@@ -1,4 +1,17 @@
 package L9MaxSliceSum;
 
 public class Solution {
+    public int solution(int[] A) {
+        int len = A.length;
+        if (len == 1) {
+            return A[0];
+        }
+        int max = A[0];
+        int temp_max = A[0];
+        for (int i = 1; i < len; i++) {
+            temp_max = Math.max(A[i], temp_max+A[i]);
+            max = Math.max(max, temp_max);
+        }
+        return max;
+    }
 }
