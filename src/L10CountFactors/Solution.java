9ba846954dbb681f7b1f65dfaf67a8994c2da2b2
caiwenjing <cai_wenjing@outlook.com>
2020-08-24 12:20:34 +0800

    pass L9 CountFactors

--- src/L10CountFactors/Solution.java
+++ src/L10CountFactors/Solution.java
@@ -1,4 +1,21 @@
 package L10CountFactors;
 
 public class Solution {
+    public int solution(int N) {
+        if (N == Integer.MAX_VALUE) {
+            return 2;
+        }
+        int count = 0;
+        int i = 1;
+        while (i * i < N) {
+            if (N % i == 0) {
+                count += 2;
+            }
+            i++;
+        }
+        if (i * i == N) {
+            count++;
+        }
+        return count;
+    }
 }
